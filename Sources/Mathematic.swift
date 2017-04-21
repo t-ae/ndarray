86ceb01a6cdf81874ba170d08a2150c09217a543
araki <araki@qoncept.co.jp>
2017-04-21 18:02:52 +0900

    Refine `add`

--- Sources/Mathematic.swift
+++ Sources/Mathematic.swift
@@ -9,29 +9,48 @@ public func +(lhs: NDArray, rhs: NDArray) -> NDArray {
 func add(_ lhs: NDArray, _ rhs: NDArray) -> NDArray {
     let (lhs, rhs) = broadcast(lhs, rhs)
     
-    let y = gatherElements(rhs, forceUniqueReference: true)
-    
-    let strDims = stridedDims(shape: lhs.shape, strides: lhs.strides)
+    let strDims = min(stridedDims(shape: lhs.shape, strides: lhs.strides),
+                      stridedDims(shape: rhs.shape, strides: rhs.strides))
     
     let majorShape = [Int](lhs.shape.dropLast(strDims))
-    let majorStrides = [Int](lhs.strides.dropLast(strDims))
     let minorShape = lhs.shape.suffix(strDims)
     
     let count = minorShape.reduce(1, *)
-    var dst = UnsafeMutablePointer(mutating: y)
     
-    let xStride = Int32(lhs.strides.last ?? 0)
+    var dst = UnsafeMutablePointer<Float>.allocate(capacity: lhs.volume)
+    defer { dst.deallocate(capacity: lhs.volume) }
     
-    for majorIndex in NDIndexSequence(shape: majorShape) {
-        let offset = indexOffset(strides: majorStrides, ndIndex: majorIndex) + lhs.baseOffset
-        let src = UnsafePointer(lhs.data) + offset
-        cblas_saxpy(Int32(count), 1,
-                    src, xStride,
-                    dst, 1)
-        dst += count
+    let lMajorStrides = [Int](lhs.strides.dropLast(strDims))
+    let rMajorStrides = [Int](rhs.strides.dropLast(strDims))
+    var lOffsets = getOffsets(shape: majorShape, strides: lMajorStrides)
+    var rOffsets = getOffsets(shape: majorShape, strides: rMajorStrides)
+    let lStride = Int32(lhs.strides.last ?? 0)
+    let rStride = Int32(rhs.strides.last ?? 0)
+    
+    let numProc = ProcessInfo.processInfo.activeProcessorCount
+    let blockSize = Int(ceil(Float(lOffsets.count) / Float(numProc)))
+    
+    DispatchQueue.concurrentPerform(iterations: numProc) { i in
+        var dstPtr = dst.advanced(by: i*blockSize*count)
+        let end = i*blockSize + min(blockSize, lOffsets.count - i*blockSize)
+        
+        guard i*blockSize < end else { // can be empty
+            return
+        }
+        
+        for oi in i*blockSize..<end {
+            let lOffset = lOffsets[oi] + lhs.baseOffset
+            let lSrc = UnsafePointer(lhs.data).advanced(by: lOffset)
+            let rOffset = rOffsets[oi] + rhs.baseOffset
+            let rSrc = UnsafePointer(rhs.data).advanced(by: rOffset)
+            vDSP_vadd(lSrc, vDSP_Stride(lStride),
+                      rSrc, vDSP_Stride(rStride),
+                      dstPtr, 1, vDSP_Length(count))
+            dstPtr += count
+        }
     }
-    
-    return NDArray(shape: lhs.shape, elements: y)
+    return NDArray(shape: lhs.shape,
+                   elements: [Float](UnsafeBufferPointer(start: dst, count: lhs.volume)))
 }
 
 // MARK: - negate
