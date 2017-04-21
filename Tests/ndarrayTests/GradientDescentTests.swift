e6135b680cd782d8efda651bfc8d1137ac864163
araki <araki@qoncept.co.jp>
2017-04-22 02:10:12 +0900

    Refine test

--- Tests/ndarrayTests/GradientDescentTests.swift
+++ Tests/ndarrayTests/GradientDescentTests.swift
@@ -5,26 +5,34 @@ import XCTest
 class GradientDescentTests: XCTestCase {
 
     func testGradientDescent() {
-        // y = 3*x^2 + 2*x + 1
+        // y = 0.3*x^2 + 0.2*x + 0.1
         
-        let xs = NDArray.linspace(low: -2, high: 2, count: 300).reshaped([-1, 1])
-        let ys = 3*xs*xs + 2*xs + 1 + NDArray.normal(mu: 0, sigma: 0.1, shape: xs.shape)
+        let xs = NDArray.linspace(low: -1, high: 1, count: 300).reshaped([-1, 1])
+        
+        // data
+        var ys = 0.3*xs*xs + 0.2*xs + 0.1
+        ys += NDArray.normal(mu: 0, sigma: 0.03, shape: xs.shape)
+        ys = ys.raveled()
         
         print("xs: \(xs.shape), ys: \(ys.shape)")
         
+        // x^2, x^1, x^0
         let features = NDArray.concat([xs*xs, xs, NDArray.ones(xs.shape)], along: 1)
         print("features: \(features.shape)")
         
-        var theta = NDArray(shape: [3], elements: [2, 2, 2])
+        var theta = NDArray(shape: [3], elements: [1, 1, 1])
         
         let alpha: Float = 0.1
         
-        for i in 0..<1000 {
+        for i in 0..<10000 {
             print("\nstep: \(i)")
-            let distance = sum(theta * features, along: 1) - ys.raveled() // shape: [300]
-            let loss = mean(exp2(distance), along: 0) / 2
+            
+            // calculate loss
+            let distance = sum(theta * features, along: 1) - ys
+            let loss = mean(distance*distance, along: 0) / 2
             print("loss: \(loss.asScalar())")
             
+            // Update parameters
             let grads = distance.reshaped([-1, 1]) * features
             print("grads: \(grads.shape)")
             let update = alpha * mean(grads, along: 0)
@@ -35,6 +43,9 @@ class GradientDescentTests: XCTestCase {
         
         print("\nanswer")
         print(theta)
+        let distance = sum(theta * features, along: 1) - ys
+        let loss = mean(distance*distance, along: 0) / 2
+        print("loss: \(loss.asScalar())")
         print("")
         
     }
