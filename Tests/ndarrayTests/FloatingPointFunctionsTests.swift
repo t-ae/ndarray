b183a474dc38a424421049b243d76fac573de229
araki <araki@qoncept.co.jp>
2017-04-23 00:52:43 +0900

    Add test

--- Tests/ndarrayTests/FloatingPointFunctionsTests.swift
+++ Tests/ndarrayTests/FloatingPointFunctionsTests.swift
@@ -18,10 +18,16 @@ class FloatingPointFunctionsTests: XCTestCase {
             XCTAssertEqual(b, NDArray(shape: [2, 2, 2], elements: [0, 4, 2, 6, 1, 5, 3, 7].map(sqrtf)))
         }
         do {
-            // not dense
+            // not dense, least stride is 1
             let a = NDArray.range(8).reshaped([2, 2, 2])[nil, 1]
             let b = sqrt(a)
             XCTAssertEqual(b, NDArray(shape: [2, 2], elements: [2, 3, 6, 7].map(sqrtf)))
+        }
+        do {
+            // not dense, least stride is not 1
+            let a = NDArray.range(8).reshaped([2, 2, 2])[nil, nil, 1]
+            let b = sqrt(a)
+            XCTAssertEqual(b, NDArray(shape: [2, 2], elements: [1, 3, 5, 7].map(sqrtf)))
         }
     }
     
