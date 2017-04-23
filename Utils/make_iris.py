eb5a9444105346f36a032f10c6fa0a21b9c164f7
araki <araki@qoncept.co.jp>
2017-04-23 16:28:21 +0900

    Add iris dataset

---
+++ Utils/make_iris.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import os
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
+iris = datasets.load_iris()
+
+
+x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.4, random_state=0)
+
+
+def array2str(array2d, spaces):
+    ret = []
+
+    for array in array2d:
+        ret.append(str(list(array)))
+
+    return (",\n" + " "*spaces).join(ret)
+
+data = f"""
+import NDArray
+
+struct Iris {{
+    static let x_train = NDArray([
+        {array2str(x_train, 8)}
+    ])
+    static let y_train = NDArray({list(y_train)})
+    
+    static let x_test = NDArray([
+        {array2str(x_test, 8)}
+    ])
+    static let y_test = NDArray({list(y_test)})
+}}
+"""[1:-1]
+
+print(data)
