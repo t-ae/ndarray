16a800be95755a850d3444c95c9095130e13298d
araki <araki@qoncept.co.jp>
2017-04-23 14:25:50 +0900

    Add matrix inverse

--- Sources/LinearAlgebra.swift
+++ Sources/LinearAlgebra.swift
@@ -1,7 +1,56 @@
 
 import Foundation
+import Accelerate
 
 /// Calculate Frobenius norm
 public func norm(_ arg: NDArray) -> Float {
     return sqrtf(sum(arg*arg).asScalar())
+}
+
+public func inv(_ arg: NDArray) throws -> NDArray {
+    precondition(arg.ndim > 1)
+    let size = arg.shape[arg.ndim-1]
+    precondition(arg.shape[arg.ndim-2] == size)
+    
+    let volume = arg.volume
+    var elements = gatherElements(arg, forceUniqueReference: true)
+    
+    let numMatrices = volume / (size*size)
+    
+    var N = __CLPK_integer(size)
+    var pivots = UnsafeMutablePointer<__CLPK_integer>.allocate(capacity: size)
+    var workspace = UnsafeMutablePointer<__CLPK_real>.allocate(capacity: size)
+    var error: __CLPK_integer = 0
+    
+    defer {
+        pivots.deallocate(capacity: size)
+        workspace.deallocate(capacity: size)
+    }
+    
+    var ptr = UnsafeMutablePointer(mutating: elements)
+    for _ in 0..<numMatrices {
+        
+        sgetrf_(&N, &N, ptr, &N, pivots, &error)
+        if error < 0 {
+            throw NDArrayInvError.IrregalValue
+        } else if error > 0 {
+            throw NDArrayInvError.SingularMatrix
+        }
+        
+        sgetri_(&N, ptr, &N, pivots, workspace, &N, &error)
+        if error < 0 {
+            throw NDArrayInvError.IrregalValue
+        } else if error > 0 {
+            throw NDArrayInvError.SingularMatrix
+        }
+        
+        ptr += size*size
+    }
+    
+    return NDArray(shape: arg.shape, elements: elements)
+}
+
+enum NDArrayInvError: Error {
+    case IrregalValue
+    case SingularMatrix
 }
--- Tests/ndarrayTests/LinearAlgebraTests.swift
+++ Tests/ndarrayTests/LinearAlgebraTests.swift
@@ -10,5 +10,33 @@ class LinearAlgebraTests: XCTestCase {
             XCTAssertEqual(norm(a), sqrtf(14))
         }
     }
+    
+    func testInvert() {
+        do {
+            let a = NDArray([[1, 0], [0, 1]])
+            XCTAssertEqual(try! inv(a), a)
+            let b = NDArray.stack([a, a, a])
+            XCTAssertEqual(try! inv(b), b)
+        }
+        do {
+            let a = NDArray([[1, 2, 3],
+                             [1, 3, 5],
+                             [2, 4, 5]])
+            XCTAssertEqualWithAccuracy(try! inv(a),
+                                       NDArray([[ 5, -2, -1],
+                                                [-5,  1,  2],
+                                                [ 2,  0, -1]]),
+                                       accuracy: 1e-5)
+        }
+        do {
+            let a = NDArray.range(8).reshaped([2, 2, 2])
+            XCTAssertEqualWithAccuracy(try! inv(a),
+                                       NDArray([[[-1.5,  0.5],
+                                                 [ 1 ,  0 ]],
+                                                [[-3.5,  2.5],
+                                                 [ 3 , -2 ]]]),
+                                       accuracy: 1e-5)
+        }
+    }
 
 }
