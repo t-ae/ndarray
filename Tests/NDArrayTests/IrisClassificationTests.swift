d3722ceda8b8c2434e5a91662c5c193f20f74b6d
araki <araki@qoncept.co.jp>
2017-04-24 16:31:58 +0900

    Fix: gradient calculations

--- Tests/NDArrayTests/IrisClassificationTests.swift
+++ Tests/NDArrayTests/IrisClassificationTests.swift
@@ -14,7 +14,7 @@ class IrisClassificationTests: XCTestCase {
         let y = toOneHot(labels)
         
         let numFeatures = x.shape[1]
-        let numTrainSamples = x.shape[0]
+        let numTrainSamples = x.shape[0] // = M
         let numOutput = y.shape[1]
         
         let labelsCount = sum(y, along: 0)
@@ -45,30 +45,30 @@ class IrisClassificationTests: XCTestCase {
             let out = softmax(h2)   // [M, 3]
             
             // back propagation
-            let d_out_h2 = out - y  // [90, 3]
+            let d_out_h2 = out - y  // [M, 3]
             
-            let d_h2_b2 = NDArray.ones(b2.shape)        // [90, 3]
-            let d_h2_h2_1 = NDArray.ones(h2_1.shape)    // [90, 3]
+            let d_h2_b2 = NDArray.ones(b2.shape)        // [M, 3]
+            let d_h2_h2_1 = NDArray.ones(h2_1.shape)    // [M, 3]
             
-            let d_h2_1_W2 = NDArray.stack([NDArray](repeating: h1, count: numOutput), newAxis: -1) // [90, 5, 4]
-            let d_h2_1_h1 = W2
+            let d_h2_1_W2 = h1 // [M, 5]
+            let d_h2_1_h1 = W2 // [M, 5]
             
             let d_h1_h1_2 = d_relu(h1_2)
             
             let d_h1_2_b1 = NDArray.ones(b1.shape) // [90, 5]
             let d_h1_2_h1_1 = NDArray.ones(h1_1.shape) // [90, 5]
             
-            let d_h1_1_W1 = NDArray.stack([NDArray](repeating: x, count: numHiddenUnits1), newAxis: -1) // [90, 4, 3]
+            let d_h1_1_W1 = x // [90, 4]
             
             // chain
             let d_out_b2 = d_h2_b2 * d_out_h2           // [90, 3]
             let d_out_h2_1 = d_h2_h2_1 * d_out_h2       // [90, 3]
-            let d_out_W2 = d_h2_1_W2 <*> d_out_h2_1.reshaped(d_out_h2_1.shape + [1])
+            let d_out_W2 = d_h2_1_W2.reshaped([90, 5, 1]) <*> d_out_h2_1.reshaped([90, 1, 3])
             let d_out_h1 = (d_h2_1_h1 <*> d_out_h2_1.reshaped(d_out_h2_1.shape + [1])).reshaped([90, 5])
             let d_out_h1_2 = d_h1_h1_2 * d_out_h1       // [90, 5]
             let d_out_b1 = d_out_h1_2 * d_h1_2_b1       // [90, 5]
             let d_out_h1_1 = d_h1_2_h1_1 * d_out_h1_2   // [90, 5]
-            let d_out_W1 = d_h1_1_W1 <*> d_out_h1_1.reshaped([90, 5, 1])
+            let d_out_W1 = d_h1_1_W1.reshaped([90, 4, 1]) <*> d_out_h1_1.reshaped([90, 1, 5])
             
             // update
             b2 -= alpha * mean(d_out_b2, along: 0)
@@ -79,7 +79,7 @@ class IrisClassificationTests: XCTestCase {
             
             if i%100 == 0 {
                 print("\nstep: \(i)")
-                let losses = -y * log(max(y - out, 1e-10))
+                let losses = -y * log(max(out, 1e-10))
                 let loss = mean(sum(losses, along: 1)).asScalar()
                 let featureLosses = sum(losses, along: 0) / labelsCount
                 print("loss: \(loss), (\(featureLosses.elements()))")
@@ -109,7 +109,7 @@ class IrisClassificationTests: XCTestCase {
             let out = softmax(h2)   // [M, 3]
             
             print("\ntest result:")
-            let losses = -y * log(max(y - out, 1e-10))
+            let losses = -y * log(max(out, 1e-10))
             let loss = mean(sum(losses, along: 1)).asScalar()
             let featureLosses = sum(losses, along: 0) / labelsCount
             print("loss: \(loss), (\(featureLosses.elements()))")
