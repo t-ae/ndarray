ad90ce7dd7c78b2200496aac3f3a1e9347f615a1
araki <araki@qoncept.co.jp>
2017-04-26 15:15:45 +0900

    Add test

--- Tests/NDArrayTests/TransformationTests.swift
+++ Tests/NDArrayTests/TransformationTests.swift
@@ -1,6 +1,6 @@
 
 import XCTest
-import NDArray
+@testable import NDArray
 
 class TransformationTests: XCTestCase {
     
@@ -21,28 +21,71 @@ class TransformationTests: XCTestCase {
     }
     
     func testFlip() {
-        let a = NDArray.range(9).reshaped([3, 3])
-        XCTAssertEqual(a.flipped(0),
-                       NDArray([[6, 7, 8],
-                                [3, 4, 5],
-                                [0, 1, 2]]))
-        
-        XCTAssertEqual(a.flipped(1),
-                       NDArray([[2, 1, 0],
-                                [5, 4, 3],
-                                [8, 7, 6]]))
-        
-        XCTAssertEqual(a.flipped(0).flipped(1),
-                       NDArray([[8, 7, 6],
-                                [5, 4, 3],
-                                [2, 1, 0]]))
-        
-        XCTAssertEqual(a.flipped(1).flipped(0),
-                       NDArray([[8, 7, 6],
-                                [5, 4, 3],
-                                [2, 1, 0]]))
-        
-        XCTAssertEqual(a.flipped(0).flipped(0), a)
-        XCTAssertEqual(a.flipped(1).flipped(1), a)
+        do {
+            let a = NDArray.range(9).reshaped([3, 3])
+            XCTAssertEqual(a.flipped(0),
+                           NDArray([[6, 7, 8],
+                                    [3, 4, 5],
+                                    [0, 1, 2]]))
+            
+            XCTAssertEqual(a.flipped(1),
+                           NDArray([[2, 1, 0],
+                                    [5, 4, 3],
+                                    [8, 7, 6]]))
+            
+            XCTAssertEqual(a.flipped(0).flipped(1),
+                           NDArray([[8, 7, 6],
+                                    [5, 4, 3],
+                                    [2, 1, 0]]))
+            
+            XCTAssertEqual(a.flipped(1).flipped(0),
+                           NDArray([[8, 7, 6],
+                                    [5, 4, 3],
+                                    [2, 1, 0]]))
+            
+            XCTAssertEqual(a.flipped(0).flipped(0), a)
+            XCTAssertEqual(a.flipped(1).flipped(1), a)
+        }
+        do {
+            let a = NDArray(shape: [3, 3],
+                            strides: [3, 0],
+                            baseOffset: 1,
+                            data: [0, 1, 2, 3, 4, 5, 6, 7, 8])
+            XCTAssertEqual(a.flipped(0),
+                           NDArray([[7, 7, 7],
+                                    [4, 4, 4],
+                                    [1, 1, 1]]))
+            XCTAssertEqual(a.flipped(1),
+                           NDArray([[1, 1, 1],
+                                    [4, 4, 4],
+                                    [7, 7, 7]]))
+            XCTAssertEqual(a.flipped(0).flipped(1),
+                           NDArray([[7, 7, 7],
+                                    [4, 4, 4],
+                                    [1, 1, 1]]))
+        }
+        do {
+            let a = NDArray(shape: [3, 3],
+                            strides: [0, 1],
+                            baseOffset: 3,
+                            data: [0, 1, 2, 3, 4, 5, 6, 7, 8])
+            XCTAssertEqual(a.flipped(0),
+                           NDArray([[3, 4, 5],
+                                    [3, 4, 5],
+                                    [3, 4, 5]]))
+            XCTAssertEqual(a.flipped(1),
+                           NDArray([[5, 4, 3],
+                                    [5, 4, 3],
+                                    [5, 4, 3]]))
+            XCTAssertEqual(a.flipped(1),
+                           NDArray([[5, 4, 3],
+                                    [5, 4, 3],
+                                    [5, 4, 3]]))
+            XCTAssertEqual(a.flipped(0).flipped(1),
+                           NDArray([[5, 4, 3],
+                                    [5, 4, 3],
+                                    [5, 4, 3]]))
+        }
     }
+    
 }
