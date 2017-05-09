b44e989b08d8f482e6c7d8468ee45a53fbdb624b
araki <araki@qoncept.co.jp>
2017-05-09 11:59:13 +0900

    Add comment

--- Sources/Select.swift
+++ Sources/Select.swift
@@ -1,10 +1,12 @@
 
 extension NDArray {
     
+    /// Create new NDArray which contains only specified rows.
     public func select(_ indices: [Int]) -> NDArray {
         return NDArray.stack(indices.map{ self[$0] })
     }
     
+    /// Create new NDArray which contains only rows fufill `predicate`.
     public func select(where predicate: (NDArray)->Bool) -> NDArray {
         return NDArray.stack(self.filter(predicate))
     }
