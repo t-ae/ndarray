273e7767e1fd9974e6df4cc49cc8b90763500f0b
araki <araki@qoncept.co.jp>
2017-05-16 12:54:46 +0900

    Fix: `select` with no rows

--- Sources/Select.swift
+++ Sources/Select.swift
@@ -3,22 +3,36 @@ extension NDArray {
     
     /// Create new NDArray which contains only specified rows.
     public func select(_ indices: [Int]) -> NDArray {
+        guard !indices.isEmpty else {
+            return NDArray.empty(shape.removing(at: 0).inserting(0, at: 0))
+        }
         return NDArray.stack(indices.map{ self[$0] })
     }
     
     /// Create new NDArray which contains only rows fufill `predicate`.
     public func select(where predicate: (NDArray)->Bool) -> NDArray {
-        return NDArray.stack(filter(predicate))
+        let filtered = filter(predicate)
+        guard !filtered.isEmpty else {
+            return NDArray.empty(shape.removing(at: 0).inserting(0, at: 0))
+        }
+        return NDArray.stack(filtered)
     }
     
     /// Create new NDArray which contains only rows fufill `predicate` with index.
     public func select(where predicate: (Int, NDArray)->Bool) -> NDArray {
-        return NDArray.stack(enumerated().flatMap { predicate($0, $1) ? $1 : nil })
+        let filtered = enumerated().flatMap { predicate($0, $1) ? $1 : nil }
+        guard !filtered.isEmpty else {
+            return NDArray.empty(shape.removing(at: 0).inserting(0, at: 0))
+        }
+        return NDArray.stack(filtered)
     }
     
     /// Create new NDArray with rows `mask == true`
     public func select(_ mask: [Bool]) -> NDArray {
         precondition(mask.count == shape[0])
+        guard mask.contains(true) else {
+            return NDArray.empty(shape.removing(at: 0).inserting(0, at: 0))
+        }
         return NDArray.stack(zip(self, mask).flatMap { $1 ? $0 : nil })
     }
     
--- Tests/NDArrayTests/SelectTests.swift
+++ Tests/NDArrayTests/SelectTests.swift
@@ -10,15 +10,21 @@ class SelectTests: XCTestCase {
                        NDArray([[0, 1, 2],
                                 [0, 1, 2],
                                 [3, 4, 5]]))
+        let noIndices: [Int] = []
+        XCTAssertEqual(a.select(noIndices), NDArray.empty([0, 3]))
         
         XCTAssertEqual(a.select { $0[1].asScalar() < 7 },
                        NDArray([[0, 1, 2], [3, 4, 5]]))
+        XCTAssertEqual(a.select { $0[1].asScalar() < 0 },
+                       NDArray.empty([0, 3]))
         
         XCTAssertEqual(a.select([true, false, false]),
                        NDArray([[0, 1, 2]]))
         XCTAssertEqual(a.select([true, false, true]),
                        NDArray([[0, 1, 2],
                                 [6, 7, 8]]))
+        let noMask: [Bool] = [false, false, false]
+        XCTAssertEqual(a.select(noMask), NDArray.empty([0, 3]))
     }
     
     func testIndices() {
