45b4fd908150faf25822a68305c4370890780eb0
araki <araki@qoncept.co.jp>
2017-05-18 13:39:11 +0900

    Add argsort

--- Sources/Sort.swift
+++ Sources/Sort.swift
@@ -12,7 +12,20 @@ public func sort(_ arg: NDArray, ascending: Bool = true) -> NDArray {
 }
 
 /// Index sort 1 dimensional NDArray.
-/* Not working now
+public func argsort(_ arg: NDArray, ascending: Bool = true) -> [Int] {
+    precondition(arg.ndim == 1)
+    if ascending {
+        return arg.enumerated()
+            .sorted { l, r in l.element.asScalar() < r.element.asScalar() }
+            .map { $0.offset }
+    } else {
+        return arg.enumerated()
+            .sorted { l, r in l.element.asScalar() >= r.element.asScalar() }
+            .map { $0.offset }
+    }
+}
+
+/* This may faster, but somehow not works
 public func argsort(_ arg: NDArray, ascending: Bool = true) -> [UInt] {
     precondition(arg.ndim == 1, "`arg` must be 1 dimensional.")
     var index = [vDSP_Length](repeating: 0, count: arg.volume)
--- Tests/NDArrayTests/SortTests.swift
+++ Tests/NDArrayTests/SortTests.swift
@@ -11,10 +11,11 @@ class SortTests: XCTestCase {
         XCTAssertEqual(sort(a, ascending: false), NDArray([6, 4, 2, 0.1, 0, -0.2, -1, -3, -5, -7]))
     }
     
-    /*
+    
     func testArgsort() {
         let a = NDArray([0, -1, 2, -3, 4, -5, 6, -7, 0.1, -0.2])
         XCTAssertEqual(argsort(a, ascending: true), [7, 5, 3, 1, 9, 0, 8, 2, 4, 6])
+        XCTAssertEqual(argsort(a, ascending: false), [6, 4, 2, 8, 0, 9, 1, 3, 5, 7])
     }
-    */
+ 
 }
