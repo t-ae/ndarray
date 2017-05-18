e6c149e53460b0ea75862fba2ddeae1d49be6339
araki <araki@qoncept.co.jp>
2017-05-18 09:46:35 +0900

    Add comment, test

--- Sources/CopySign.swift
+++ Sources/CopySign.swift
@@ -1,6 +1,7 @@
 
 import Accelerate
 
+/// Change the sign of `magnitude` to that of `sign`, element-wise.
 public func copySign(magnitude: NDArray, sign: NDArray) -> NDArray {
     return apply(magnitude, sign, vvcopysignf)
 }
--- Tests/NDArrayTests/CopySignTests.swift
+++ Tests/NDArrayTests/CopySignTests.swift
@@ -5,11 +5,18 @@ import NDArray
 class CopySignTests: XCTestCase {
     
     func testCopySign() {
-        let a = NDArray([0, -1, -1, 2])
-        let b = NDArray([1, -3, 2, -5])
-        
-        XCTAssertEqual(copySign(magnitude: a, sign: b), NDArray([0, -1, 1, -2]))
-        XCTAssertEqual(copySign(magnitude: b, sign: a), NDArray([1, -3, -2, 5]))
+        do {
+            let a = NDArray([0, -1, -1, 2])
+            let b = NDArray([1, -3, 2, -5])
+            
+            XCTAssertEqual(copySign(magnitude: a, sign: b), NDArray([0, -1, 1, -2]))
+            XCTAssertEqual(copySign(magnitude: b, sign: a), NDArray([1, -3, -2, 5]))
+        }
+        do {
+            XCTAssertEqual(1/copySign(magnitude: NDArray(scalar: 0), sign: NDArray(scalar: 1)),
+                                      NDArray(scalar: Float.infinity))
+            XCTAssertEqual(1/copySign(magnitude: NDArray(scalar: 0), sign: NDArray(scalar: -1)),
+                                      NDArray(scalar: -Float.infinity))
+        }
     }
-    
 }
