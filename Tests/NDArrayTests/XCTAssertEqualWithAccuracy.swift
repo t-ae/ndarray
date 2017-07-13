251c8a67f8984e7e41738aae5878e17bf50da2ea
araki <araki@qoncept.co.jp>
2017-07-13 12:21:07 +0900

    Refine test

--- Tests/NDArrayTests/XCTAssertEqualWithAccuracy.swift
+++ Tests/NDArrayTests/XCTAssertEqualWithAccuracy.swift
@@ -2,14 +2,18 @@
 import XCTest
 @testable import NDArray
 
-func XCTAssertEqualWithAccuracy(_ expression1: NDArray, _ expression2: NDArray, accuracy: Float) {
+func XCTAssertEqualWithAccuracy(_ expression1: NDArray,
+                                _ expression2: NDArray,
+                                accuracy: Float,
+                                file: StaticString = #file,
+                                line: UInt = #line) {
     XCTAssertEqual(expression1.shape, expression2.shape)
     
     let elements1 = gatherElements(expression1)
     let elements2 = gatherElements(expression2)
     for (e1, e2) in zip(elements1, elements2) {
         if fabsf(e1-e2) > accuracy {
-            XCTFail("Assertion failed:\n\(elements1.asArray())\n\(elements2.asArray())")
+            XCTFail("Not nearly equal:\n\(elements1.asArray())\n\(elements2.asArray())", file: file, line: line)
             return
         }
     }
