bdad9cc7720bd511a1a6ad00a3a3b2f7df61e7be
araki <araki@qoncept.co.jp>
2017-10-10 12:21:25 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,30 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "NDArray",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "NDArray",
+            targets: ["NDArray"]),
+    ],
     dependencies: [
-        .Package(url: "https://github.com/t-ae/xorswift.git", Version("0.0.9"))
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/t-ae/xorswift.git", from: "0.0.10")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "NDArray",
+            dependencies: ["Xorswift"],
+            path: "Sources"),
+        .testTarget(
+            name: "NDArrayTests",
+            dependencies: ["NDArray"]),
     ]
 )
