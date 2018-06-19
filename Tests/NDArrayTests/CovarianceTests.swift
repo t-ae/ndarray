42274621ff9613c715246f3852191614841ea8ba
araki <araki@qoncept.co.jp>
2018-06-19 18:02:59 +0900

    Add CovarianceTests

---
+++ Tests/NDArrayTests/CovarianceTests.swift
@@ -0,0 +1,20 @@
+import XCTest
+@testable import NDArray
+
+class CovarianceTests: XCTestCase {
+
+    func testCovariance() {
+        
+        let matrix = cov(Iris.x_train.transposed())
+        
+        XCTAssertEqual(matrix, matrix.transposed())
+        
+        let answer = NDArray([[ 0.78304317, -0.02713582,  1.47912342,  0.59903701],
+                              [-0.02713582,  0.18066173, -0.27110865, -0.10254815],
+                              [ 1.47912342, -0.27110865,  3.45583208,  1.44405182],
+                              [ 0.59903701, -0.10254815,  1.44405182,  0.6398222 ]])
+        
+        XCTAssertEqual(matrix, answer, accuracy: 1e-5)
+    }
+    
+}
--- Tests/NDArrayTests/XCTAssertEqualWithAccuracy.swift
+++ Tests/NDArrayTests/XCTAssertEqualWithAccuracy.swift
@@ -1,12 +1,11 @@
-
 import XCTest
 @testable import NDArray
 
 func XCTAssertEqual(_ expression1: NDArray,
-                                _ expression2: NDArray,
-                                accuracy: Float,
-                                file: StaticString = #file,
-                                line: UInt = #line) {
+                    _ expression2: NDArray,
+                    accuracy: Float,
+                    file: StaticString = #file,
+                    line: UInt = #line) {
     XCTAssertEqual(expression1.shape, expression2.shape, file: file, line: line)
     
     let elements1 = gatherElements(expression1)
