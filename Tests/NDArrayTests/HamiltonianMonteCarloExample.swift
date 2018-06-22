d55074eea6a0186c699e075d388d62ecc14dbde3
araki <araki@qoncept.co.jp>
2018-06-22 17:36:31 +0900

    Change test

--- Tests/NDArrayTests/HamiltonianMonteCarloExample.swift
+++ Tests/NDArrayTests/HamiltonianMonteCarloExample.swift
@@ -2,54 +2,6 @@ import XCTest
 import NDArray
 
 class HamiltonianMonteCarloExample: XCTestCase {
-
-    static let mu = NDArray([[30], [0]])
-    static let sigma = NDArray([[1, 0.5],
-                                [0.5, 1]])
-    static var sigma_inv = try! inv(sigma)
-    
-    var mu: NDArray {
-        return HamiltonianMonteCarloExample.mu
-    }
-    var sigma: NDArray {
-        return HamiltonianMonteCarloExample.sigma
-    }
-    var sigma_inv: NDArray {
-        return HamiltonianMonteCarloExample.sigma_inv
-    }
-    
-    func log_normal(x: NDArray) -> NDArray {
-        return -0.5 * (x-mu).transposed() |*| sigma_inv |*| (x-mu)
-    }
-    
-    func d_log_normal(x: NDArray) -> NDArray {
-        return -sigma_inv |*| (x-mu)
-    }
-    func hamiltonian(state: NDArray, velocity: NDArray) -> NDArray {
-        return (velocity.transposed() |*| velocity)/2 - log_normal(x: state)
-    }
-    
-    func sample(state: NDArray, epsilon: Float, steps: Int) -> NDArray {
-        let velocity = NDArray.normal(mu: 0, sigma: 1, shape: [2, 1])
-        
-        var new_state = state
-        var new_velocity = velocity
-        
-        for _ in 0..<steps {
-            // leap frog
-            new_velocity += -epsilon/2 * -d_log_normal(x: new_state)
-            new_state += epsilon * new_velocity
-            new_velocity += -epsilon/2 * -d_log_normal(x: new_state)
-        }
-        
-        let r = exp(hamiltonian(state: new_state, velocity: new_velocity) - hamiltonian(state: state, velocity: velocity))
-        if r.asScalar() > NDArray.uniform(low: 0, high: 1, shape: []).asScalar() {
-            return new_state
-        } else {
-            return state
-        }
-    }
-    
     func testHamiltonianMonteCarlo() {
         
         let sampleNum = 10000
@@ -84,4 +36,43 @@ class HamiltonianMonteCarloExample: XCTestCase {
 //        }
     }
 
+}
+
+private let mu = NDArray([[30], [0]])
+private let sigma = NDArray([[1, 0.5],
+                            [0.5, 1]])
+private var sigma_inv = try! inv(sigma)
+
+
+private func log_normal(x: NDArray) -> NDArray {
+    return -0.5 * (x-mu).transposed() |*| sigma_inv |*| (x-mu)
+}
+
+private func d_log_normal(x: NDArray) -> NDArray {
+    return -sigma_inv |*| (x-mu)
+}
+
+private func hamiltonian(state: NDArray, velocity: NDArray) -> NDArray {
+    return (velocity.transposed() |*| velocity)/2 - log_normal(x: state)
+}
+
+private func sample(state: NDArray, epsilon: Float, steps: Int) -> NDArray {
+    let velocity = NDArray.normal(mu: 0, sigma: 1, shape: [2, 1])
+    
+    var new_state = state
+    var new_velocity = velocity
+    
+    for _ in 0..<steps {
+        // leap frog
+        new_velocity += -epsilon/2 * -d_log_normal(x: new_state)
+        new_state += epsilon * new_velocity
+        new_velocity += -epsilon/2 * -d_log_normal(x: new_state)
+    }
+    
+    let r = exp(hamiltonian(state: new_state, velocity: new_velocity) - hamiltonian(state: state, velocity: velocity))
+    if r.asScalar() > NDArray.uniform(low: 0, high: 1, shape: []).asScalar() {
+        return new_state
+    } else {
+        return state
+    }
 }
